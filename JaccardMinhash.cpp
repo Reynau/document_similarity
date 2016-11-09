aaf026a36c660f35248525d645b2ab9d5c68a8a8
Reynau <xavi.lopez.reynau@gmail.com>
2016-11-09 17:12:57 +0100

    Add hash function

--- JaccardMinhash.cpp
+++ JaccardMinhash.cpp
@@ -3,7 +3,22 @@
 
 using namespace std;
 
-double MinhashSimilitude() {
-    cout << "No implementado "; return 0;
+double MinhashSimilitude(vector<string> &doc1, vector<string> &doc2) {
+    unsigned long val = hash('Holi');
+    unsigned long val2 = hash('oHil');
+    cout << val << " " << val << endl;
+    return 0;
 }
 
+unsigned long hash(string str)
+{
+    unsigned long hash = 5381;
+    int c;
+
+    for (int i = 0; i < str.size(); ++i) {
+        c = str[i];
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+    }
+
+    return hash;
+}
