906c0def211b26a86aed6a3a90aef08b1e4120f6
Xavier Lopez Reynau <xavi.lopez.reynau@gmail.com>
2016-11-14 19:40:32 +0100

    Update JaccardMinhash to a class and uses new HashFunctions class

--- JaccardMinhash.cpp
+++ JaccardMinhash.cpp
@@ -1,17 +1,16 @@
 #include "JaccardMinhash.h"
 
 #include <stdlib.h>     /* srand, rand */
+#include<fstream>
 
 using namespace std;
 
 const bool comments = false;
-const vector<int> primes = {2, 23, 59, 97, 137, 179, 227, 269, 313, 367, 419, 461, 509, 571, 617, 661, 727, 773,
-                            829, 883, 947, 1009, 1051, 1103, 1171, 1129, 1289, 1327, 1427, 1471, 1523, 1579, 1621};
 
 /* Universal Hashing:
  * https://en.wikipedia.org/wiki/Universal_hashing#Hashing_strings
  * */
-double hash_f(string str, int p) {
+double JaccardMinhash::hash_f(string str, int p) {
     unsigned long long h = 5381;
     for (unsigned int i = 0; i < str.size(); ++i)
         h = ((h << 5) + h) + str[i]; // (h * 33 + c)
@@ -22,11 +21,11 @@ double hash_f(string str, int p) {
  * To calculate hmin(S), you pass every member of S through the hash function h,
  * and find the member that gives the lowest result.
  * */
-string calculateMinHash (vector<string> &doc, unsigned int i) {
-    double hmin1 = hash_f(doc[0], primes[i]);
+string JaccardMinhash::calculateMinHash (vector<string> &doc, unsigned int prime) {
+    double hmin1 = this->hashFunctions.stringHash(doc[0], prime);
     string min1 = doc[0];
     for (unsigned int w = 1; w < doc.size(); ++w) {
-        double h = hash_f(doc[w], primes[i]);
+        double h = this->hashFunctions.stringHash(doc[w], prime);
         if (hmin1 > h) { min1 = doc[w]; hmin1 = h; }
     }
     return min1;
@@ -37,7 +36,8 @@ string calculateMinHash (vector<string> &doc, unsigned int i) {
  * To calculate hmin(S), you pass every member of S through the hash function h,
  * and find the member that gives the lowest result.
  * */
-double MinhashSimilitude(vector<string> &doc1, vector<string> &doc2, unsigned int k) {
+double JaccardMinhash::MinhashSimilitude(unsigned int k) {
+    vector<unsigned int> primes = this->hashFunctions.getPrimeNumbers();
     // Pre-evaluation: K needs to be < primes.size()
     if (k >= primes.size()) {
         k = (unsigned int)primes.size();
@@ -50,11 +50,11 @@ double MinhashSimilitude(vector<string> &doc1, vector<string> &doc2, unsigned in
         if (comments) cout << "Working on hash function #" << i << "." << endl;
         // 1.2. Calculates hmin for doc1
         if (comments) cout << "Calculating hmin for doc1... ";
-        string min1 = calculateMinHash(doc1, i);
+        string min1 = calculateMinHash(this->doc1, primes[i]);
         if (comments) cout << "FINISHED! hmin word is \"" << min1 << "\"." << endl;
         // 1.3. Calculates hmin for doc2
         if (comments) cout << "Calculating hmin for doc2... ";
-        string min2 = calculateMinHash(doc2, i);
+        string min2 = calculateMinHash(this->doc2, primes[i]);
         if (comments) cout << "FINISHED! hmin word is \"" << min2 << "\"." << endl;
         // 2. Calculate how many hmin coincided --> y
         if (min1 == min2) {
--- JaccardMinhash.h
+++ JaccardMinhash.h
@@ -4,10 +4,23 @@
 #include <iostream>
 #include <vector>
 
+#include "HashFunctions.h"
+
 using namespace std;
 
-double hash_f(string str, int p);
-string calculateMinHash (vector<string> &doc, unsigned int i);
-double MinhashSimilitude(vector<string> &doc1, vector<string> &doc2, unsigned int k);
+class JaccardMinhash {
+private:
+    HashFunctions hashFunctions;
+    vector<string> doc1;
+    vector<string> doc2;
+public:
+    JaccardMinhash(HashFunctions &hashFunctions,vector<string> &doc1, vector<string> &doc2) :
+        hashFunctions(hashFunctions), doc1(doc1), doc2(doc2) {};
+
+    double hash_f(string str, int p);
+    string calculateMinHash (vector<string> &doc, unsigned int i);
+    double MinhashSimilitude(unsigned int k);
+};
+
 
 #endif
