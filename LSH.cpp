fb6056eedaf63814afccb880eefefbc41d775f2e
Reynau <xavi.lopez.reynau@gmail.com>
2016-11-15 19:51:25 +0100

    New LSH implementation

--- LSH.cpp
+++ LSH.cpp
@@ -3,3 +3,101 @@
 //
 
 #include "LSH.h"
+#include "JaccardMinhash.h"
+
+double LSH::generate() {
+    cout << "Dividing docs in K-Shingles...";
+    divideKShingles(this->doc1);
+    divideKShingles(this->doc2);
+    cout << " FINISHED" << endl;
+
+    cout << "Calculating MinHash signatures...";
+    vector<unsigned int> mhSignature1 = calculateMinhashSignature(this->doc1);
+    vector<unsigned int> mhSignature2 = calculateMinhashSignature(this->doc2);
+    cout << " FINISHED" << endl;
+
+    cout << "Calculating signature Matrix.." << endl;
+    vector<vector<unsigned int>> signMat = {mhSignature1, mhSignature2};
+    cout << "Calculating bucket map.." << endl;
+    map<unsigned long, pair<bool, bool>> bucketMap = generateBucketMap(signMat);
+    cout << "Calculating jaccard similarity.." << endl;
+    return calculateJaccardSimilarity(bucketMap);
+}
+
+void LSH::divideKShingles(vector<string> &doc) {
+    vector<string> sDoc;
+    unsigned long final = doc.size() - this->k;
+    for (int i = 0; i < final; i++){
+        string s = doc[i];
+        for(int j=1; j<k; j++) s.append(doc[i+j]);
+    }
+    doc = sDoc;
+}
+
+vector<unsigned int> LSH::calculateMinhashSignature(vector<string> &doc) {
+    vector<unsigned int> primes = this->hashFunctions.getPrimeNumbers();
+    vector<unsigned int> minhash = vector<unsigned int>(this->t);
+    for (int i = 0; i < this->t; ++i)
+        minhash[i] = calculateMinHash(doc, primes[i]);
+    return minhash;
+}
+
+map<unsigned long, pair<bool, bool>> LSH::generateBucketMap(vector<vector<unsigned int>> signatureMatrix) {
+    unsigned int sizeB = this->t / this->b;
+
+    map<unsigned long, pair<bool, bool>> bucketMap;
+    // DOC1
+    for (int i = 0; i < this->t; ++i) { // Per cada funcio de hash
+        vector<unsigned int> bucket = vector<unsigned int>(sizeB);
+        for (int j = 0; j < sizeB; ++j) {
+            unsigned int pos = i*sizeB + j;
+            unsigned int hash = signatureMatrix[0][pos];
+            bucket[j] = hash;
+        }
+
+        unsigned long hash = this->hashFunctions.HashVector(bucket);    // Generate hash for vector
+        pair<bool, bool> mapElement = bucketMap[hash];                  // Get the pair of bools
+        mapElement.first = true;                                        // Set a true for that doc
+        bucketMap[hash] = mapElement;                                   //Saves changes
+    }
+
+    // DOC2
+    for (int i = 0; i < this->t; ++i) { // Per cada funcio de hash
+        vector<unsigned int> bucket = vector<unsigned int>(sizeB);
+        for (int j = 0; j < sizeB; ++j) {
+            unsigned int pos = i*sizeB + j;
+            unsigned int hash = signatureMatrix[1][pos];
+            bucket[j] = hash;
+        }
+
+        unsigned long hash = this->hashFunctions.HashVector(bucket);    // Generate hash for vector
+        pair<bool, bool> mapElement = bucketMap[hash];                  // Get the pair of bools
+        mapElement.second = true;                                       // Set a true for that doc
+        bucketMap[hash] = mapElement;                                   //Saves changes
+    }
+    return bucketMap;
+}
+
+double LSH::calculateJaccardSimilarity(map<unsigned long, pair<bool, bool>> &bucketMap) {
+    unsigned int intersection = 0;
+    unsigned int unionn = 0;
+
+    typedef map<unsigned long, pair<bool, bool>>::iterator it_type;
+    for(it_type iterator = bucketMap.begin(); iterator != bucketMap.end(); iterator++) {
+        pair<bool, bool> element = iterator->second;
+        if(element.first and element.second) ++intersection;
+        ++unionn;
+    }
+
+    return (double)intersection/(double)unionn;
+}
+
+unsigned int LSH::calculateMinHash (vector<string> &set, unsigned int prime) {
+    double hmin1 = this->hashFunctions.stringHash(set[0], prime);
+    string min1 = set[0];
+    for (unsigned int w = 1; w < set.size(); ++w) {
+        double h = this->hashFunctions.stringHash(set[w], prime);
+        if (hmin1 > h) { min1 = set[w]; hmin1 = h; }
+    }
+    return hmin1;
+}
--- LSH.h
+++ LSH.h
@@ -5,9 +5,32 @@
 #ifndef DOCUMENT_SIMILARITY_LSH_H
 #define DOCUMENT_SIMILARITY_LSH_H
 
+#include <string>
+#include <map>
+
+#include "HashFunctions.h"
 
 class LSH {
+private:
+    HashFunctions hashFunctions;
+    vector<string> doc1;
+    vector<string> doc2;
 
+    unsigned int k; // Tamaño single
+    unsigned int t; // Numero de hash functions
+    unsigned int b; // Numero de bands
+
+    unsigned int calculateMinHash (vector<string> &set, unsigned int prime);
+public:
+    LSH(HashFunctions &hashFunctions, vector<string> &doc1, vector<string> &doc2,
+    unsigned int k, unsigned int t, unsigned int b) :
+            hashFunctions(hashFunctions), doc1(doc1), doc2(doc2), k(k), t(t), b(b) {}
+
+    double generate();
+    void divideKShingles(vector<string> &doc);
+    vector<unsigned int> calculateMinhashSignature(vector<string> &doc);
+    map<unsigned long, pair<bool, bool>> generateBucketMap(vector<vector<unsigned int>> signatureMatrix);
+    double calculateJaccardSimilarity(map<unsigned long, pair<bool, bool>> &bucketMap);
 };
 
 
