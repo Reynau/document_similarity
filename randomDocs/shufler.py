77d1a3fb76e1acb9cdf335e9fd4bac0d315ec270
Obscurs <ferris202@gmail.com>
2016-11-15 05:02:29 +0100

    added folder with docs with 50 words shufled

---
+++ randomDocs/shufler.py
@@ -0,0 +1,152 @@
+import random
+x = [
+"remunerability",
+"jemmying",
+"mutiny",
+"lexicography",
+"coinsurer",
+"accusingly",
+"unshriven",
+"repercussion",
+"subdenticulate",
+"uncirculating",
+"rags",
+"typikon",
+"spiritualise",
+"dopa",
+"outbraved",
+"rearticulation",
+"clingingly",
+"manicheus",
+"counseling",
+"pedalfer",
+"uncordial",
+"slideway",
+"eniopeus",
+"phew",
+"snuggeries",
+"parrying",
+"nonnitrogenous",
+"forkedly",
+"harriet",
+"turgot",
+"adenectomy",
+"wheeled",
+"racehorse",
+"respite",
+"ricercare",
+"welsh",
+"papaya",
+"duit",
+"axiological",
+"copp",
+"redding",
+"spruance",
+"collegiateness",
+"manqu",
+"lutheranism",
+"anticonservative",
+"candolle",
+"recharge",
+"examine",
+"recidivist"]
+
+
+random.shuffle(x)
+thefile = open('doc1.txt', 'w')
+for item in x:
+  thefile.write("%s\n" % item)
+
+random.shuffle(x)
+thefile = open('doc2.txt', 'w')
+for item in x:
+  thefile.write("%s\n" % item)
+
+random.shuffle(x)
+thefile = open('doc3.txt', 'w')
+for item in x:
+  thefile.write("%s\n" % item)
+
+random.shuffle(x)
+thefile = open('doc4.txt', 'w')
+for item in x:
+  thefile.write("%s\n" % item)
+
+random.shuffle(x)
+thefile = open('doc5.txt', 'w')
+for item in x:
+  thefile.write("%s\n" % item)
+
+random.shuffle(x)
+thefile = open('doc6.txt', 'w')
+for item in x:
+  thefile.write("%s\n" % item)
+
+random.shuffle(x)
+thefile = open('doc7.txt', 'w')
+for item in x:
+  thefile.write("%s\n" % item)
+
+random.shuffle(x)
+thefile = open('doc8.txt', 'w')
+for item in x:
+  thefile.write("%s\n" % item)
+random.shuffle(x)
+thefile = open('doc9.txt', 'w')
+for item in x:
+  thefile.write("%s\n" % item)
+
+random.shuffle(x)
+thefile = open('doc10.txt', 'w')
+for item in x:
+  thefile.write("%s\n" % item)
+
+random.shuffle(x)
+thefile = open('doc11.txt', 'w')
+for item in x:
+  thefile.write("%s\n" % item)
+
+random.shuffle(x)
+thefile = open('doc12.txt', 'w')
+for item in x:
+  thefile.write("%s\n" % item)
+
+random.shuffle(x)
+thefile = open('doc13.txt', 'w')
+for item in x:
+  thefile.write("%s\n" % item)
+
+random.shuffle(x)
+thefile = open('doc14.txt', 'w')
+for item in x:
+  thefile.write("%s\n" % item)
+
+random.shuffle(x)
+thefile = open('doc15.txt', 'w')
+for item in x:
+  thefile.write("%s\n" % item)
+
+random.shuffle(x)
+thefile = open('doc16.txt', 'w')
+for item in x:
+  thefile.write("%s\n" % item)
+
+random.shuffle(x)
+thefile = open('doc17.txt', 'w')
+for item in x:
+  thefile.write("%s\n" % item)
+
+random.shuffle(x)
+thefile = open('doc18.txt', 'w')
+for item in x:
+  thefile.write("%s\n" % item)
+
+random.shuffle(x)
+thefile = open('doc19.txt', 'w')
+for item in x:
+  thefile.write("%s\n" % item)
+
+random.shuffle(x)
+thefile = open('doc20.txt', 'w')
+for item in x:
+  thefile.write("%s\n" % item)
